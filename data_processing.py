d1c36ea78600e4d4b9c7727f7c2a162c2b95bd90
NProof <34260623+NProof@users.noreply.github.com>
2022-01-02 16:52:11 +0800

    featureSelecting( )

--- data_processing.py
+++ data_processing.py
@@ -2,6 +2,7 @@
 # 用於處理資料的函式庫
 
 import pandas as pd
+from feature_selection import PFA
 
 # 
 def deduplicate(df, subject):
@@ -13,3 +14,19 @@ def convertTime(timestamp):
     minute = timestamp.apply(lambda _ : _.minute)
     time = pd.DataFrame(data={'d':date.values, 'h':hour.values, 'm':minute.values}, index=timestamp)
     return time
+
+def featureSelecting(f_df_raw):
+    X = f_df_raw
+    pfa = PFA(n_features=4)
+    pfa.fit(X)
+    
+    # To get the transformed matrix
+    X = pfa.features_
+    
+    # To get the column indices of the kept features
+    column_indices = pfa.indices_
+    
+    column_names = f_df_raw.columns[column_indices]
+    if '冰機Q(sum)' not in column_names:
+        column_names = column_names.insert(0, '冰機Q(sum)')
+    return column_names
