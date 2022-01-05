0908e7fa8aebed17ae72ccf284bd1fd5b28b9173
NProof <34260623+NProof@users.noreply.github.com>
2022-01-05 13:25:53 +0800

    def dataPreProcessing2

--- main2.py
+++ main2.py
@@ -16,9 +16,9 @@ if __name__ == '__main__':
     history = history.iloc[:25000]
     history = data_processing.deduplicate(history, subject='時間')
     # add timestamp
-    t_sp_h = history['時間'].apply(lambda _ : pd.to_datetime(_))
-    time_h = data_processing.convertTime(t_sp_h)
-    history.index = t_sp_h
+    history['時間'] = history['時間'].apply(lambda _ : pd.to_datetime(_))
+    time_h = data_processing.convertTime(history['時間'])
+    history = history.set_index("時間")
     
     # PART TEST WITH MODIFIED ERROR
 
@@ -27,7 +27,6 @@ if __name__ == '__main__':
     err = err.iloc[:25000]
     err = data_processing.deduplicate(err, subject='time')
     # add timestamp
-    t_sp_e = err['時間'].apply(lambda _ : pd.to_datetime(_))
-    time_e = data_processing.convertTime(t_sp_e)
-    err.index = t_sp_e
-    
+    err['time'] = err['time'].apply(lambda _ : pd.to_datetime(_))
+    time_e = data_processing.convertTime(err['time'])
+    err = err.set_index('time')
